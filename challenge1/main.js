e0191272d155412109cb3f20b3409165e8418c4d
nawan44 <rachmat.d.gunawan@gmail.com>
2020-11-24 17:19:55 +0700

    challenge javascript 1 done

---
+++ challenge1/main.js
@@ -0,0 +1,20 @@
+function grade(){
+    var nilai = document.getElementById("formNilai").input1.value;
+    var p_kontainer = document.getElementById("container");
+    if( nilai >= 90 ){
+        p_kontainer.innerHTML = "A";
+    } 
+    else if ((nilai >= 80) && (nilai  <= 89)) {
+        p_kontainer.innerHTML = "B";
+    }
+    else if ((nilai >= 70) && (nilai  <= 79))  {
+        p_kontainer.innerHTML = "C";
+    }
+    else if((nilai >= 60) && (nilai  <= 69))  {
+        p_kontainer.innerHTML = "D";
+    }
+    else if (nilai <= 59) {
+        p_kontainer.innerHTML = "E";
+    }
+
+}
