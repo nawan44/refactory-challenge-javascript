ac5667c7c82d4def6daf3e8e60f5ab4e54ea0b07
Rachmat Gunawan <rachmat.d.gunawan@gmail.com>
2021-03-18 00:07:17 +0700

    convert to Lower Case

--- challenge5/main.js
+++ challenge5/main.js
@@ -1,12 +1,20 @@
-function palindrom(nilai1) {
+function palindrom() {
     let nilai1 = document.getElementById("formNilai").input1.value;
-    let p_kontainer = document.getElementById("container");
-    let nilai2 = nilai1.split('').reverse().join('');
-    if (nilai1 === nilai2) {
-        p_kontainer.innerHTML = "palindrom";
+    let nilai2 = nilai1.toLowerCase();
+let nilai3 = document.getElementById("container1").innerHTML= nilai2 ;
+nilai2.innerHTML = nilai3;
+console.log(nilai3)
+
+
+    let nilai4 = nilai3.split('').reverse().join('');
+    
+    let p_kontainer2 = document.getElementById("container2");
+    
+    if (nilai3 === nilai4) {
+        p_kontainer2.innerHTML = "palindrom";
     }
     else {
-        p_kontainer.innerHTML = " Bukan palindrom";
+        p_kontainer2.innerHTML = " Bukan palindrom";
     }
 }
 
