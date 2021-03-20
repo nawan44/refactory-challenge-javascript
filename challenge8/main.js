9d846b81af11ff79e46f5e877dcb79a52699579c
Rachmat Gunawan <rachmat.d.gunawan@gmail.com>
2021-03-20 15:36:32 +0700

    solve challenge 8

--- challenge8/main.js
+++ challenge8/main.js
@@ -1,14 +1,29 @@
-function reverseText() {
-    const a = document
-        .getElementById("input1")
-        .value;
-    const b = a
-        .split(' ')
-        .reverse()
-    const newText = b.join(' ')
-
-    document
-        .getElementById("container")
-        .innerHTML = newText;
-
+function swap(arr,first,second){
+    let temp=arr[first];
+    arr[first]=arr[second];
+    arr[second]=temp;
 }
+
+function buble() {
+ 
+let str=document.getElementById("input").value;
+let arr;
+if(str.length===0){
+    arr=new Array;
+}
+else{
+    arr=str.match(/\d+/g).map(Number);
+}
+let len=arr.length,
+i,j,stop;
+for(i=0;i<len;i++){
+    for(j=0,stop=len-i;j<stop;j++){
+        if(arr[j]>arr[j+1]){
+           swap(arr,j,j+1);
+        }
+    }
+}
+document
+.getElementById("container")
+.innerHTML = arr;
+ }
