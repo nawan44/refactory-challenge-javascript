e21bc56fcc7ef993f6439b98ac9ed3fc2b1546f4
Rachmat Gunawan <rachmat.d.gunawan@gmail.com>
2021-03-23 23:58:51 +0700

    template challenge 9

---
+++ challenge9/main.js
@@ -0,0 +1,11 @@
+function dataSort() {
+  let numbers = document.getElementById("input").value;
+
+
+
+
+  document.getElementById("data").innerHTML = numbers;
+  document.getElementById("data_sort").innerHTML = arr;
+
+
+}
