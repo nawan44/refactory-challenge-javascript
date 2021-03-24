70569e9721411f72f69c1feae2bea117580c6f26
Rachmat Gunawan <rachmat.d.gunawan@gmail.com>
2021-03-24 20:22:21 +0700

    challenge 9

--- challenge9/main.js
+++ challenge9/main.js
@@ -1,11 +1,38 @@
-function dataSort() {
+let items = [];
+function arrayInPairs() {
   let numbers = document.getElementById("input").value;
 
 
+let jbb = numbers.split(" ").map(Number)
+// console.log("fruitsString", jbb)
+// console.log(typeof jbb);
+// console.log(Array.isArray(jbb));
+const arrs = '[' + jbb.join(", ") + ']';
+// console.log("fruitsString", arrs)
+// console.log(typeof arrs);
+// console.log(Array.isArray(arrs));
+
+  
+
+  let arraySort = [...numbers].sort((a, b) => a - b);
+  let totalArrays = [];
+  console.log(arraySort);
+
+  for (let i = 0; i < arraySort.length; i += 2) {
+    let sliceArray = i + 2;
+    totalArrays.push(arraySort.slice(i, sliceArray));
+  }
+  console.log(totalArrays.join(", "));
+
+  // let A = totalArrays.map(Number)
+  let B = '[' +totalArrays + ']';
 
 
+// console.log(A); 
+  
+  
   document.getElementById("data").innerHTML = numbers;
-  document.getElementById("data_sort").innerHTML = arr;
+  document.getElementById("array").innerHTML = arrs;
 
-
+  document.getElementById("result").innerHTML = B;
 }
