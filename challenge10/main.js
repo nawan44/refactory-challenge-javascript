4869b25eae9a985e3cdcf95b0347372fb0de8513
Rachmat Gunawan <rachmat.d.gunawan@gmail.com>
2021-03-27 20:56:13 +0700

    challenge 10 -

--- challenge10/main.js
+++ challenge10/main.js
@@ -1,13 +1,13 @@
-function swap(arr, first, second) {
-  let temp = arr[first];
-  arr[first] = arr[second];
-  arr[second] = temp;
-}
-function arrayInPairs() {
-  //Data Input
-  let numbers = document.getElementById("input").value;
 
-  document.getElementById("data").innerHTML = numbers;
-  
-  // document.getElementById("result").innerHTML = (totalArrays)
+function convertTime() {
+  //Data Input
+  let numbers = document.getElementById("data_hours").value;
+
+ function convertHoursToMinute(){
+    resulltConvertHoursToMinute = numbers *60
+return resulltConvertHoursToMinute
+  }
+  document.getElementById("data_minute").innerHTML = numbers;
+
+  document.getElementById("result").innerHTML = convertHoursToMinute(numbers)
 }
