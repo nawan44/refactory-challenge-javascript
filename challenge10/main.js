b963a0619d9c91bbc576a0fa7dd4137c489f23f2
Rachmat Gunawan <rachmat.d.gunawan@gmail.com>
2021-03-27 22:16:53 +0700

    challenge 10 - multiple choice

--- challenge10/main.js
+++ challenge10/main.js
@@ -1,12 +1,32 @@
-function convertTime() {
+// document.getElementById("Selector").onchange = convert;
+
+function convert() {
   //Data Input
   let numbers = document.getElementById("data_hours").value;
+  console.log(numbers);
 
-  function convertHoursToMinute() {
-    resulltConvertHoursToMinute = numbers * 60;
-    return resulltConvertHoursToMinute;
+  if (Selector.value === "convertHoursToMinute") {
+    let select = " Hours";
+    resulltConvertHoursToMinute = numbers * 60 + " Minute ";
+
+    document.getElementById("data_input").innerHTML = numbers + select;
+    document.getElementById("result").innerHTML = resulltConvertHoursToMinute;
+    console.log(resulltConvertHoursToMinute);
+  } else if (Selector.value === "convertMinuteToHours") {
+    let select = " Minute";
+
+    resulltConvertMinuteToHours = numbers / 60 + " Hours ";
+    document.getElementById("data_input").innerHTML = numbers + select;
+
+    document.getElementById("result").innerHTML = resulltConvertMinuteToHours;
+    console.log(resulltConvertMinuteToHours);
+  } else if (Selector.value === "convertMinuteToYear") {
+    let select = " Minute";
+
+    resulltConvertMinuteToYear = numbers / 60 / 24 / 365 + " Year ";
+    document.getElementById("data_input").innerHTML = numbers + select;
+
+    document.getElementById("result").innerHTML = resulltConvertMinuteToYear;
+    console.log(resulltConvertMinuteToYear);
   }
-  document.getElementById("data_minute").innerHTML = numbers;
-
-  document.getElementById("result").innerHTML = convertHoursToMinute(numbers);
 }
