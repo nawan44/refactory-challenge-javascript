fbdf647539cf3d4714c7a5a32542fa264328a0a9
Rachmat Gunawan <rachmat.d.gunawan@gmail.com>
2021-03-27 20:56:38 +0700

    challenge 10 - convertHoursToMinute

--- challenge10/main.js
+++ challenge10/main.js
@@ -1,13 +1,12 @@
-
 function convertTime() {
   //Data Input
   let numbers = document.getElementById("data_hours").value;
 
- function convertHoursToMinute(){
-    resulltConvertHoursToMinute = numbers *60
-return resulltConvertHoursToMinute
+  function convertHoursToMinute() {
+    resulltConvertHoursToMinute = numbers * 60;
+    return resulltConvertHoursToMinute;
   }
   document.getElementById("data_minute").innerHTML = numbers;
 
-  document.getElementById("result").innerHTML = convertHoursToMinute(numbers)
+  document.getElementById("result").innerHTML = convertHoursToMinute(numbers);
 }
